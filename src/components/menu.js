a14217f0f3a8d95442d38e850c0da74e3397dba5
thinker <123@qq.com>
2020-06-15 12:41:42 +0800

    rename

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,9 +1,8 @@
 import React from "react";
-import {BrowserRouter as Router,Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export default function Menu(){
     return (
-
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 import Home from '../pages/home/home'
 import About from '../pages/about/about'
